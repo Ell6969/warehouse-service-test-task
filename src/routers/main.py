c5845980f6376deabf18212fa8e16c285038806a
Vyacheslav <129338963+Ell6969@users.noreply.github.com>
2025-08-12 17:42:31 +0300

    Update main.py
    naming

--- src/routers/main.py
+++ src/routers/main.py
@@ -15,7 +15,7 @@ router = APIRouter(prefix="", tags=["API"])
 
 @router.get("/movements/{movement_id}")
 @cache(expire=100, key_builder=path_param_key_builder)
-async def get_gg(movement_id: UUID) -> SGetMovementByIdResult:
+async def get_movement(movement_id: UUID) -> SGetMovementByIdResult:
     """
     Возвращает информацию о перемещении по его ID, включая отправителя, получателя, время, прошедшее между отправкой и приемкой, и разницу в количестве товара.
     """
@@ -28,7 +28,7 @@ async def get_gg(movement_id: UUID) -> SGetMovementByIdResult:
 
 @router.get("/warehouses/{warehouse_id}/products/{product_id}")
 @cache(expire=100, key_builder=path_param_key_builder)
-async def get_ggg(warehouse_id: UUID, product_id: UUID) -> SGetProductWarehouseByIdResult:
+async def get_remains_product_warehouse(warehouse_id: UUID, product_id: UUID) -> SGetProductWarehouseByIdResult:
     """
     Возвращает информацию текущем запасе товара в конкретном складе.
     """
